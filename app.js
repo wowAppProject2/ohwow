f9d45014b99407c59dba2a3e8886ed5131372480
Joe Gencarelli <jp.genc@gmail.com>
2022-05-22 18:15:07 -0400

    Appended video/timestamp and wow number for user's selection with respective styles. Removed console logs and cleaned up code

--- app.js
+++ app.js
@@ -10,63 +10,65 @@ wowApp.init=()=>{
     // QuerySelect the form element
     wowApp.formSelect=document.querySelector('form');
 }
-
+          
 // Method that populates the dropdown menu with API call
-
+          
 wowApp.populateDropDown=()=>{
     fetch("https://owen-wilson-wow-api.herokuapp.com/wows/movies")
     .then((response)=>{
-        if (response.ok) {
-            return response.json()
-        } 
-        // Error handling if response has no data to return
-        else {
-            throw new Error('Wow there is nothing here');
-        }   
-    })
-
+    if (response.ok) {
+        return response.json()
+    } 
+    // Error handling if response has no data to return
+    else {
+        throw new Error('Wow there is nothing here');
+    }   
+})
+          
     .then ((wowData)=>{
         // Populate dropdown menu using array returned from API call
         // select the select element
         const movieSelector=document.querySelector('#moviesDropDown');
         // Use a forEach loop to populate each option element
         wowData.forEach((movies)=>{
-            // Create the option element
+        // Create the option element
             const movieChoice=document.createElement('option');
             // Set the textContent equal to the item in the array
             movieChoice.textContent = movies;
             // Append each option element to the select element
             movieSelector.append(movieChoice);
         })
-
+          
     })
-
+          
     .catch((error)=>{
-        // Alert user with error message if data does not exist
+    // Alert user with error message if data does not exist
         alert(error);
     })
-
+          
 }
-
+          
 wowApp.movieClick = ()=>{
-
-    // Add an event listner to the form element
-    wowApp.formSelect=addEventListener('click', (e)=>{
-
+          
+// Add an event listner to the form element
+    wowApp.formSelect=addEventListener('change', (e)=>{
+      
     // Save the innerText of the option clicked to a property in the namespace object
-    wowApp.userChoice=e.target.innerText;
-    // if(wowApp.userChoice==="Choose a movie")
-    // {
-    //     alert("I thought you wanted to hear a wow?")
-    // }
+    wowApp.userChoice=e.target.value;
+    console.log(wowApp.userChoice);
 
+              // if(wowApp.userChoice==="Choose a movie")
+              // {
+              //     alert("I thought you wanted to hear a wow?")
+              // }
+          
     // Call the getWows function, passing the wowApp.userChoice property as an argument
     wowApp.getWows(wowApp.userChoice);
-
+          
     })
-
+          
 }
-
+          
 // Function declaration for getWows method, that takes the wowApp.userChoice as a paramter
 wowApp.getWows = movieWow=>{
     fetch(`https://owen-wilson-wow-api.herokuapp.com/wows/random?movie=${movieWow}`)
@@ -74,54 +76,76 @@ wowApp.getWows = movieWow=>{
         if (response.ok) {
             return response.json()
         } 
-        // Error handling if response has no data to return
+    // Error handling if response has no data to return
         else {
             throw new Error('Wow there is nothing here');
         }   
     })
-
-        .then((wowData) => {
-            console.log(wowData);
-            console.log(movieWow);
-            wowApp.createCard(wowData);
-        })
-
-        .catch((error) => {
-            // Alert user with error message if data does not exist
-            alert(error);
-        })
-
-
+          
+    .then((wowData) => {
+        wowApp.createCard(wowData);
+    })
+    .catch((error) => {
+    // Alert user with error message if data does not exist
+        alert(error);
+    })
 }
-
+          
 //A function declaration to create card elements populate the list
 wowApp.createCard =(wowData)=> {
-    const firstOption = document.querySelector('#firstOption')
-    console.log("this is first option", firstOption);
 
+    // Create div to hold video and text content 
+    const videoAndTextContainer=document.createElement('div');
+    // Add a class to the container holding the video and text content
+    videoAndTextContainer.classList.add('videoTextContainer');
+    
+    // Create div to hold all text content
+    const singleCardContainer=document.createElement('div');
+    // Add a class to the container holding all the text content
+    singleCardContainer.classList.add('singleCardContainer');
 
+    // Select the main container which will flex each video and text container
+    // NOTE: RENAME CONTAINER
+    const wowText=document.querySelector('.cardTextContainer');
+
+    // Create wow#, timestamp and full quote elements
+    const wowNumber = document.createElement('p');
+    const timeStamp= document.createElement('p');
+    const wowQuote = document.createElement ('p');
+
+    // Add a class to the paragraph holding the full quote
+    wowQuote.classList.add("fullQuote");
+
+    // Set the text content of each element to their respective properties in the object
+    wowNumber.textContent=`Wow # : ${wowData[0].current_wow_in_movie}`;
+    timeStamp.textContent=`Time : ${wowData[0].timestamp}`;
+    wowQuote.textContent=`"${wowData[0].full_line}"`;
+
+    //Find the video source URL and save to a varibale 
     const videoSource = wowData[0].video["1080p"];
-    console.log("this is a video source", videoSource);
-
-    // Query select the card container to later display the info
-    const movieCard = document.querySelector('.movieDropdownContainer');
-
-    // Create movie element within the main card container
+     
+    // Create video element element
     wowApp.generatedVideo = document.createElement('video');
+    // Create the source element, which is the child of the video element
     wowApp.generateSource = document.createElement('source');
-
-    // Set the attributes to make the video and controlls appear
+          
+    // Set the attributes to make the video and controls appear
     wowApp.generateSource.setAttribute('src', videoSource);
-    wowApp.generatedVideo.setAttribute('controls', 'controls')
-
-    //Append working source to video, then video to the movie card
+    wowApp.generatedVideo.setAttribute('controls', 'controls');
+          
+    //Append working source to video
     wowApp.generatedVideo.append(wowApp.generateSource);
-    movieCard.append(wowApp.generatedVideo);
-    console.log(wowApp.generateSource);
+  
+    //Append the text to the singleCardContainer
+    singleCardContainer.append(wowNumber, timeStamp, wowQuote);
+
+    // Append the video and singleTextContainer to the videoAndTextContainer
+    videoAndTextContainer.append(wowApp.generatedVideo,singleCardContainer);
+
+    // Append the videoAndTextContainer to the MAIN CONTAINER (update name when class is changed)
+    wowText.append(videoAndTextContainer);
 
 }
-
+          
 // Call the init method
-wowApp.init();
-
-
+wowApp.init(); 
